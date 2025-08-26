91c2d862dea176bec17a72f08cb02453d781eb34
Vladislav Ploskov <vlad.plskv@gmail.com>
2025-08-26 22:56:13 +0300

    use base path from env

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import solidPlugin from 'vite-plugin-solid';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+	base: process.env.BASE_PATH ?? '/',
 	plugins: [solidPlugin(), tailwindcss(), tsconfigPaths()],
 	server: {
 		port: 3000,
